488fb7c7270411ea1edcd714fe087ca48ecd74ce
adamguk <adam.e.garnham@gmail.com>
2026-07-16 09:20:32 +0100

    FEAT: Updated COPY-INTO to improve performance

--- dags/scripts/populate_snowflake_raw.sql
+++ dags/scripts/populate_snowflake_raw.sql
@@ -1,5 +1,5 @@
 COPY INTO FINTECH.RAW.RAW_TRANSACTIONS
-    FROM @MY_S3_STAGE
+    FROM @MY_S3_STAGE/generated_transactions_{{ ds }}.csv
     PATTERN = '.*transactions_.*\.csv'
     FILE_FORMAT = (FORMAT_NAME = 'CSV_FILE_FORMAT')
     FORCE = FALSE; 
